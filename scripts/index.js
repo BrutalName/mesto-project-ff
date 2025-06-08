d08a7c4524bffe6b3657668c140974fff3d10952
Viktor <BrutalName@yandex.by>
2025-06-09 01:26:35 +0300

    Проектная 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const CardTempl = document.querySelector('#card-template').content
+
 // @todo: DOM узлы
+
+const CardsList = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
 
+function addCard(CardImg,CardTitle) {
+    const CardTemplClone = CardTempl.querySelector('.card').cloneNode(true)
+    const ButtonDelete = CardTemplClone.querySelector('.card__delete-button')
+
+    CardTemplClone.querySelector('.card__image').src = CardImg
+    CardTemplClone.querySelector('.card__image').alt = CardTitle
+    CardTemplClone.querySelector('.card__title').textContent = CardTitle
+
+    ButtonDelete.addEventListener('click', CardDelete)
+    
+    CardsList.append(CardTemplClone);
+}
+
 // @todo: Функция удаления карточки
 
+function CardDelete (evt) {
+    const TargetCard = evt.target.parentElement
+    TargetCard.remove()
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (Card) {
+    addCard(Card.link, Card.name)
+})
